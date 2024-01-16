e9575452ff1760d0d484a222222ab825b15833a6
lvho <lvho@tma.com.vn>
2024-01-16 14:49:03 +0700

    no message

--- export-invoice-frontend-react/src/components/Layout/components/Header/index.js
+++ export-invoice-frontend-react/src/components/Layout/components/Header/index.js
@@ -1,12 +1,31 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Header.module.scss';
+import logo from '~/assets/images/logo-tma.webp'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={logo} alt="logo-tma"></img>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search keyword..." spellCheck={false}></input>
+                    {/* <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button> */}
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+
+                </div>
+            </div>
         </header>
     );
 }
